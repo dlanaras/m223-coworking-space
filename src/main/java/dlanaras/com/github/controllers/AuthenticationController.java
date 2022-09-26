256a220c0af22a34b9b5718d4a99e280420b08ac
dlanaras <dimitrislanaras04@outlook.com>
2022-09-26 14:05:19 +0000

    done: authentication controller done

---
+++ src/main/java/dlanaras/com/github/controllers/AuthenticationController.java
@@ -0,0 +1,53 @@
+package dlanaras.com.github.controllers;
+
+import javax.inject.Inject;
+import javax.persistence.EntityExistsException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import dlanaras.com.github.exceptions.InvalidLoginException;
+import dlanaras.com.github.models.User;
+import dlanaras.com.github.models.dto.Login;
+import dlanaras.com.github.services.UserService;
+
+
+public class AuthenticationController {
+
+    @Inject
+    private UserService userService;
+
+    @POST
+    @Path("/register")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response register(User user) {
+        try {
+            String token = userService.register(user);
+            return Response.ok(token).build();
+        } catch (EntityExistsException e) {
+            System.out.println(e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        } catch (Exception e) {
+            System.out.println(e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        } 
+    }
+
+    @POST
+    @Path("/login")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response login(Login login) {
+        try {
+            String token = userService.login(login);
+            return Response.ok(token).build();
+        } catch (InvalidLoginException e) {
+            System.out.println(e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+}
