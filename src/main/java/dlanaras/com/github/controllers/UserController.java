69bca6910f40409ac184d3dab828690efc012b32
dlanaras <dimitrislanaras04@outlook.com>
2022-09-26 14:05:05 +0000

    done: usercontroller done

--- src/main/java/dlanaras/com/github/controllers/UserController.java
+++ src/main/java/dlanaras/com/github/controllers/UserController.java
@@ -1,5 +1,73 @@
 package dlanaras.com.github.controllers;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.TransactionRequiredException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import dlanaras.com.github.exceptions.NullValueException;
+import dlanaras.com.github.models.User;
+import dlanaras.com.github.services.UserService;
+
+@Path("/users")
 public class UserController {
-    
+
+    @Inject
+    UserService userService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> index() {
+        return userService.findAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(User user) {
+        try {
+            User createdUser = userService.createUser(user);
+            return Response.ok(createdUser).build();
+        } catch (Exception e) {
+            System.out.println(e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateUser(User user) {
+        try {
+            return Response.ok(userService.updateUser(user)).build();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        } catch (TransactionRequiredException e) {
+            System.out.println(e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    @Path("/{id}")
+    @DELETE
+    public Response deleteUser(Long id) {
+        try {
+            userService.deleteUser(id);
+            return Response.noContent().build();
+        } catch (NullValueException e) {
+            System.out.println(e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
 }
