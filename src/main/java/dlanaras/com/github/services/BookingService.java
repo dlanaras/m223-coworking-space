938d63d1ebca0a47471d7c1df65e20a9f4e538dd
dlanaras <dimitrislanaras04@outlook.com>
2022-09-26 13:19:42 +0000

    done: simple bookingservice

--- src/main/java/dlanaras/com/github/services/BookingService.java
+++ src/main/java/dlanaras/com/github/services/BookingService.java
@@ -1,5 +1,44 @@
 package dlanaras.com.github.services;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import dlanaras.com.github.models.Booking;
+import dlanaras.com.github.models.User;
+
+@ApplicationScoped
 public class BookingService {
-    
+    @Inject
+    private EntityManager entityManager;
+
+    public List<Booking> findAll() {
+        var query = entityManager.createQuery("FROM Booking", Booking.class);
+        return query.getResultList();
+    }
+
+    public List<Booking> findAll(int userId) {
+        var query = entityManager.createQuery("SELECT b FROM Booking b JOIN User u ON b.user=u WHERE u.id= :userId");
+        query.setParameter("userId", userId);
+        return query.getResultList();
+    }
+
+    @Transactional
+    public Booking createBooking(Booking booking) {
+        entityManager.persist(booking);
+        return booking;
+    }
+
+    @Transactional
+    public Booking updateBooking(Booking booking) {
+        return entityManager.merge(booking);
+    }
+
+    public void deleteBooking(Long id) {
+        var entity = entityManager.find(Booking.class, id);
+        entityManager.remove(entity);
+    }
 }
