e11ddaec04facba0624d3990329a8265d87082c9
dlanaras <dimitrislanaras04@outlook.com>
2022-09-26 11:44:47 +0000

    done: booking model

--- src/main/java/dlanaras/com/github/models/Booking.java
+++ src/main/java/dlanaras/com/github/models/Booking.java
@@ -1,5 +1,95 @@
 package dlanaras.com.github.models;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 public class Booking {
-    
+    @Id
+    @Column(name = "booking_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(nullable = false)
+    private Float dayPrice;
+
+    @Column(nullable = true)
+    private Boolean isAccepted;
+
+    @Column(nullable = false)
+    private Boolean needsLaptop;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Float getDayPrice() {
+        return dayPrice;
+    }
+
+    public void setDayPrice(Float dayPrice) {
+        this.dayPrice = dayPrice;
+    }
+
+    public Boolean getIsAccepted() {
+        return isAccepted;
+    }
+
+    public void setIsAccepted(Boolean isAccepted) {
+        this.isAccepted = isAccepted;
+    }
+
+    public Boolean getNeedsLaptop() {
+        return needsLaptop;
+    }
+
+    public void setNeedsLaptop(Boolean needsLaptop) {
+        this.needsLaptop = needsLaptop;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
