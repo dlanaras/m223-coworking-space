977c8f703ac2b3f8127727e0e30e5ba8be18c26d
dlanaras <dimitrislanaras04@outlook.com>
2022-09-28 07:10:23 +0000

    done: token generator should work as intended

---
+++ src/main/java/dlanaras/com/github/services/TokenService.java
@@ -0,0 +1,20 @@
+package dlanaras.com.github.services;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.eclipse.microprofile.jwt.Claims;
+
+import dlanaras.com.github.models.User;
+import io.smallrye.jwt.build.Jwt;
+
+public class TokenService {
+    public String createToken(User user) {
+        String token = Jwt.issuer("https://github.com/dlanaras")
+                .upn(user.getId().toString())
+                .groups(new HashSet<>(Arrays.asList(user.isAdmin() ? "Admin" : "User")))
+                .claim(Claims.email.name(), user.getEmail())
+                .sign();
+        return token;
+    }
+}
